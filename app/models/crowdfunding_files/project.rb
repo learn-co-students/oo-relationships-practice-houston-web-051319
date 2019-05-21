923e0f2fef5579c0fe064080d5a8894b63b3bb96
Roysan Easo <easo.roysan@gmail.com>
2019-05-21 16:19:51 -0500

    Airbnb and Crowdfunding Done

--- app/models/crowdfunding_files/project.rb
+++ app/models/crowdfunding_files/project.rb
@@ -13,7 +13,8 @@ class Project
 
     def new_pledge(user,pledge_amount)
         new_pledge = Pledge.new(user,self,pledge_amount)
-        current_fund += new_pledge.pledge_amount
+        self.current_fund += new_pledge.pledge_amount
+        new_pledge
     end
     
     def self.all
@@ -28,28 +29,32 @@ class Project
 
     def self.above_goal
         self.all.select do |each_project|
-            each_project.current_fund <= each_project.goal
+            each_project.current_fund >= each_project.goal
+        end
+    end
+
+    def self.pledged_projects
+        Pledge.all.map do |each_pledge|
+            each_pledge.project
         end
     end
 
     def self.most_backers
-        pledged_projects = Pledge.all.map do |each_pledge|
-            each_pledge.project
-        end
-        most_backers = pledged_projects[0]
-        highest_pledge_count = pledged_projects.count[most_backers]
+        most_backers = nil
+        highest_pledge_count = 0
         tied_backers = []
-        pledged_projects.all.each do |each_project|
-            if highest_pledge_count == pledged_projects.count(each_project)
+        self.pledged_projects.each do |each_project|
+            if highest_pledge_count == self.pledged_projects.count(each_project)
                 tied_backers << each_project
-            elsif highest_pledge_count <= pledged_projects.count(each_project)
+            elsif highest_pledge_count < self.pledged_projects.count(each_project)
                 most_backers = each_project
+                highest_pledge_count = self.pledged_projects.count(each_project)
                 tied_backers.clear
                 tied_backers << most_backers
             end
         end
         if tied_backers.count > 1
-            return tied_backers
+            return tied_backers.uniq
         else
             return most_backers
         end
--- tools/console.rb
+++ tools/console.rb
@@ -6,23 +6,42 @@ end
 
 #Airbnb Instances
 
-big_house = Listing.new("Houston")
-small_house = Listing.new("Houston")
-shit_house = Listing.new("Dallas")
-cool_house = Listing.new("Austin")
+# big_house = Listing.new("Houston")
+# small_house = Listing.new("Houston")
+# shit_house = Listing.new("Dallas")
+# cool_house = Listing.new("Austin")
 
-roy = Guest.new("Roy")
-bob = Guest.new("Bob")
-john = Guest.new("John")
-charley = Guest.new("Charley")
+# roy = Guest.new("Roy")
+# bob = Guest.new("Bob")
+# john = Guest.new("John")
+# charley = Guest.new("Charley")
 
-biniss = Trip.new("Business",small_house,roy)
-vacay = Trip.new("Vacation",cool_house,roy)
-college = Trip.new("College",cool_house,charley)
-trash = Trip.new("Trash",shit_house,john)
-rich = Trip.new("Rich", big_house, bob)
+# biniss = Trip.new("Business",small_house,roy)
+# vacay = Trip.new("Vacation",cool_house,roy)
+# college = Trip.new("College",cool_house,charley)
+# trash = Trip.new("Trash",shit_house,john)
+# rich = Trip.new("Rich", big_house, bob)
+
+#Crowdfunding Instances
+
+roy = User.new("Roy")
+bob = User.new("Bob")
+john = User.new("John")
+charley = User.new("Charley")
+
+big_house = Project.new(roy,"Big",1000)
+small_house = Project.new(roy,"Small",300)
+shit_house = Project.new(john,"Shit",50)
+cool_house = Project.new(charley,"Cool",700)
+
+pledge1 = big_house.new_pledge(bob,2000)
+pledge2 = small_house.new_pledge(charley,200)
+pledge3 = cool_house.new_pledge(roy,100)
+pledge4 = cool_house.new_pledge(bob,1000)
+pledge5 = small_house.new_pledge(bob,100)
 
 
-
+#Project.most_backers
 binding.pry
+
 puts "Done"
